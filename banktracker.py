96703799e09deb814a6ca46de498e324d6c9adc3
Daniel Bevan <daniel.j.bevan96@gmail.com>
2022-08-14 23:09:36 +0200

    Added click to browse file uploading. Added loading screen as file is plugged into Google Sheets. Optimized data entry based on API limits. Nearly feature complete.

--- banktracker.py
+++ banktracker.py
@@ -1,10 +1,19 @@
 import csv
+import os.path
+from pathlib import Path
+import tempfile
 import eel
+import tkinter as tk
 from sre_constants import CATEGORY_LOC_NOT_WORD
 import gspread
 import time
 from decimal import *
-from datetime import datetime
+from datetime import datetime as dt
+
+screen_width = tk.Tk().winfo_screenwidth()
+screen_height = tk.Tk().winfo_screenheight()
+win_width = 1280
+win_height = 720
 
 # Change Decimal module to have only 2 decimal places as these values are currency
 getcontext().prec = 2
@@ -13,10 +22,27 @@ getcontext().prec = 2
 transactions = []
 categories = []
 
+tmp_path = tempfile.gettempdir()
+tmp_fileName = "tmp.csv"
+completeName = os.path.join(tmp_path, tmp_fileName)
+
 # Store all the rows extracted from .csv so they can be uploaded
+def writeToTemp(fileContent):
+    lines = fileContent.splitlines()
+    print("Writing tmp file...")
+    f = open(completeName, "w")
+    f.seek(0)
+    for line in lines[:-1]:
+        f.write(line)
+        f.write('\n')
+    f.write(lines[-1])
+    f.close()
+
 @eel.expose
 def startSheetCreation(file, sheetName):
-    rows = readCSV(file)
+    writeToTemp(file)
+    rows = readCSV(completeName)
+    eel.updateProgressBar(10, "Creating Sheet...")
     createSheet(sheetName, rows)
 
 # Function for reading the .csv file returns list of transactions
@@ -26,7 +52,7 @@ def readCSV(file):
         # Iterate through each row of the .csv file
         for row in csv_reader:
             # Change the format of the date in the .csv to a more human readable format
-            date = str(datetime.strptime(row[2], '%m/%d/%Y'))
+            date = str(dt.strptime(row[2], '%m/%d/%Y'))
             name = row[4]
             category = row[5]
             # Store each unique category
@@ -46,24 +72,114 @@ def readCSV(file):
 
 # Function that creates a table of each unique category
 # so that you can track which categories cost you the most
-def insertCategories(wks, startRow):
+def insertCategories(wks, startRow, progress):
+    addedProgress = progress / len(categories)
+    print("Writing categories...")
+    offset = 0
     for category in categories:
         wks.insert_row([category], int(startRow))
+        time.sleep(1)
+        wks.update(f'B2', f'=SUMIF(C:C,INDIRECT("A{(len(categories) + 1) - offset}"),D:D)', raw=False)
+        time.sleep(1)
+        wks.format(f'B2', {
+            "numberFormat": {
+                "type": "CURRENCY"
+            }
+        })
+        eel.updateProgressBar(addedProgress, "Inserting Categories...")
+        offset = offset + 1
         time.sleep(2)
+    print("Categories done!")
+
+def updateFormatting(wks, rows):
+    
+    wks.update('F6', 'Total Income')
+    wks.update('F7', f'=SUMIF(B2:B{len(categories) + 1}, ">0", B2:B{len(categories) + 1})', value_input_option='USER_ENTERED')
+    wks.update('G6', 'Total Expenses')
+    wks.update('G7', f'=SUMIF(B2:B{len(categories) + 1}, "<0", B2:B{len(categories) + 1})', value_input_option='USER_ENTERED')
+    wks.update('H6', 'Profit/ Loss')
+    wks.update('H7', '=SUM(F7+G7)', value_input_option='USER_ENTERED')
+    wks.format('A1:B1', {
+        "backgroundColor": {
+            "red": 0.6,
+            "green": 0.6,
+            "blue": 0.6 
+        },
+        "textFormat": {
+            "bold": "True",
+            "fontSize": 14
+        }
+    })
+    wks.format(f'A{len(categories) + 3}:D{len(categories) + 3}', {
+        "backgroundColor": {
+            "red": 0.6,
+            "green": 0.6,
+            "blue": 0.6 
+        },
+        "textFormat": {
+            "bold": "True",
+            "fontSize": 14
+        }
+    })
+    wks.format('F6:H6', {
+        "backgroundColor": {
+            "red": 0.6,
+            "green": 0.6,
+            "blue": 0.6 
+        },
+        "textFormat": {
+            "bold": "True",
+            "fontSize": 14
+        }
+    })
+    wks.format('F7:H7', {
+        "numberFormat": {
+            "type": "CURRENCY"
+        }
+    })
 
 # Function that inputs all transactions into another table
-def insertExpenses(wks, startRow, rows):
+def insertExpenses(wks, startRow, rows, progress):
+    addedProgress = progress / len(rows)
+    offset = 0
+    print("Writing expenses...")
     for row in reversed(rows):
         wks.insert_row([row[0], row[1], row[2], row[3]], int(startRow))
+        time.sleep(1)
+        wks.update(f'A{(len(categories) + 4)}', row[0], value_input_option='USER_ENTERED')
+        time.sleep(1)
+        wks.update(f'D{(len(categories) + 4)}', row[3], raw=False)
+        time.sleep(1)
+        wks.format(f'A{(len(categories) + 4)}', {
+            "numberFormat": {
+                "type": "DATE"
+            }
+        })
+        wks.format(f'D{(len(categories) + 4)}', {
+            "numberFormat": {
+                "type": "CURRENCY"
+            }
+        })
+        eel.updateProgressBar(addedProgress, "Inserting Expenses...")
         time.sleep(2)
+    print("Expenses done!")
 
 # Function that creates a new sheet in the linked Google Sheets project
 def createSheet(sheetName, rows):
     worksheet = sh.add_worksheet(title=sheetName, rows=200, cols=20)
     worksheet.insert_row(["Expense Name", "Total"], 1)
-    insertCategories(worksheet, 2)
+    eel.updateProgressBar(10, "Inserting Categories...")
+    catRows = len(categories) + len(rows)
+    catProgress = 70 * (len(categories) / catRows)
+    rowProgress = 70 * (len(rows) / catRows)
+    insertCategories(worksheet, 2, catProgress)
     worksheet.insert_row(["Date", "Description", "Category", "Amount (USD)"], len(categories) + 3)
-    insertExpenses(worksheet, len(categories) + 4, rows)
+    eel.updateProgressBar(0, "Inserting Expenses...")
+    insertExpenses(worksheet, len(categories) + 4, rows, rowProgress)
+    eel.updateProgressBar(0, "Adding base formatting...")
+    updateFormatting(worksheet, rows)
+    eel.updateProgressBar(10, "Adding base formatting...")
+    eel.done()
 
 # Connect service account, open the correct Google Sheets project
 sa = gspread.service_account()
@@ -76,4 +192,4 @@ sh = sa.open("Bank Tracker")
 # Push all arguments into main function to be used if necessary
 if __name__ == "__main__":
     eel.init('web')
-    eel.start('index.html', size=(1280,720), position=(0,0))
+    eel.start('index.html', size=(win_width,win_height), position=((screen_width / 2) - (win_width / 2), (screen_height / 2) - (win_height / 2)))
--- web/script.js
+++ web/script.js
@@ -1,21 +1,52 @@
+// DOM elements
 const fileUploadImg = document.querySelector(".file-upload");
 const dropPrompt = document.querySelector(".drop-zone__prompt");
 const dropTt = document.querySelector(".drop-zone__tooltip");
 const body = document.querySelector('body');
+let progressBar = document.getElementsByClassName('progress-bar')[0];
+
+let currentProgress = 0;
+
+// Initialize DOM elements globally for easy cleanup
+let sheetSubmitBtn = null;
+let sheetBtnContainer = null;
+let sheetInputField = null;
+let successCheckImg = null;
+let dropUploaded = null;
+let inputElement = null;
+let dropZoneElement = null;
+let loadingTooltip = null;
+let loadingContainer = null;
 
 document.querySelectorAll(".drop-zone__input").forEach(inputElement => {
-    const dropZoneElement = inputElement.closest(".drop-zone");
+    let fileHandle;
+    dropZoneElement = inputElement.closest(".drop-zone");
+
+    dropZoneElement.addEventListener("click", async e => {
+        [fileHandle] = await window.showOpenFilePicker();
+
+        if(fileHandle.kind === 'file') {
+            let file = await fileHandle.getFile()
+            let text = await file.text()
+            inputElement.files[0] = file;
+            updateThumbnail(dropZoneElement, text, file.name);
+        }
+    });
 
     dropZoneElement.addEventListener("dragover", e => {
         if(inputElement.files.length == 0){
             e.preventDefault();
             dropZoneElement.classList.add("drop-zone--over");
+            fileUploadImg.classList.remove("image-normal-filter");
+            fileUploadImg.classList.add("image-drag-filter");
         }
     });
 
     ["dragleave", "dragend"].forEach(type => {
         dropZoneElement.addEventListener(type, e => {
             dropZoneElement.classList.remove("drop-zone--over");
+            fileUploadImg.classList.remove("image-drag-filter");
+            fileUploadImg.classList.add("image-normal-filter");
         });
     });
 
@@ -25,13 +56,13 @@ document.querySelectorAll(".drop-zone__input").forEach(inputElement => {
         console.log('Dropped');
 
         const fileHandlesPromises = [...e.dataTransfer.items].filter((item) => item.kind === 'file').map((item) => item.getAsFileSystemHandle());
-        console.log(fileHandlesPromises);
 
         for await (const handle of fileHandlesPromises) {
             let file = await handle.getFile();
+            let text = await file.text();
             if (handle.kind === 'file'){
                 inputElement.files[0] = file;
-                updateThumbnail(dropZoneElement, file, file.name)
+                updateThumbnail(dropZoneElement, text, file.name)
             }
         }
 
@@ -44,11 +75,13 @@ function updateThumbnail(dropZoneElement, file, fileName) {
     fileUploadImg.remove();
     dropPrompt.remove();
     dropTt.remove();
-    let successCheckImg = document.createElement('img');
+
+    successCheckImg = document.createElement('img');
     successCheckImg.src = 'success_check.svg';
     dropZoneElement.appendChild(successCheckImg);
     successCheckImg.classList.add("success-check");
-    let dropUploaded = document.createElement('div');
+
+    dropUploaded = document.createElement('div');
     dropZoneElement.appendChild(dropUploaded);
     dropUploaded.dataset.label = fileName;
     dropUploaded.classList.add("drop-zone__file-uploaded");
@@ -58,17 +91,17 @@ function updateThumbnail(dropZoneElement, file, fileName) {
 
 function createInputFields(file){
 
-    let sheetNameContainer = document.createElement('div');
+    sheetNameContainer = document.createElement('div');
     body.appendChild(sheetNameContainer);
     sheetNameContainer.classList.add("sheet-name__container");
 
-    let sheetInputLabel = document.createElement('label');
+    sheetInputLabel = document.createElement('label');
     sheetNameContainer.appendChild(sheetInputLabel);
     sheetInputLabel.setAttribute("for","sheet-name-input");
     sheetInputLabel.classList.add("form-label")
     sheetInputLabel.textContent = "Sheet Name:";
 
-    let sheetInputField = document.createElement('input');
+    sheetInputField = document.createElement('input');
     sheetNameContainer.appendChild(sheetInputField);
     sheetInputField.classList.add("form__field");
     sheetInputField.setAttribute("type","text");
@@ -76,16 +109,71 @@ function createInputFields(file){
     sheetInputField.setAttribute("placeholder","Sheet Name");
     sheetInputField.setAttribute("id", "sheet-name-input")
 
-    let sheetBtnContainer = document.createElement('div');
+    sheetBtnContainer = document.createElement('div');
     body.appendChild(sheetBtnContainer);
     sheetBtnContainer.classList.add("submit-container");
 
-    let sheetSubmitBtn = document.createElement('button');
+    sheetSubmitBtn = document.createElement('button');
     sheetBtnContainer.appendChild(sheetSubmitBtn);
     sheetSubmitBtn.classList.add("submit-btn");
     sheetSubmitBtn.textContent = "SUBMIT";
     sheetSubmitBtn.setAttribute("type","submit");
     sheetSubmitBtn.addEventListener("click", e => {
-        eel.startSheetCreation(file.name, sheetInputField.value);
+        deletePage();
+        setupNewPage();
+        eel.startSheetCreation(file, sheetInputField.value);
     });
+}
+
+function deletePage(){
+    sheetBtnContainer.remove();
+    dropZoneElement.remove();
+    sheetNameContainer.remove();
+}
+
+function setupNewPage() {
+    loadingContainer = document.createElement('div');
+    body.appendChild(loadingContainer);
+    loadingContainer.classList.add("loading-container");
+
+    let uploadTitle = document.createElement('span');
+    loadingContainer.appendChild(uploadTitle);
+    uploadTitle.classList.add("upload-title");
+    uploadTitle.textContent = "Uploading...";
+
+    progressBar = document.createElement('div');
+    loadingContainer.appendChild(progressBar);
+    progressBar.classList.add("progress-bar");
+
+    loadingTooltip = document.createElement('span');
+    loadingContainer.appendChild(loadingTooltip);
+    loadingTooltip.classList.add("loading-tooltip");
+    loadingTooltip.textContent = "Reading .csv file...";
+}
+
+eel.expose(updateProgressBar);
+function updateProgressBar(addedProgress, newToolTip){
+    const computedStyle = getComputedStyle(progressBar);
+    const progress = parseFloat(computedStyle.getPropertyValue('--progress')) || 0;
+    loadingTooltip.textContent = newToolTip;
+    progressBar.style.setProperty('--progress', progress + addedProgress);
+}
+
+eel.expose(done);
+function done(){
+    loadingContainer.remove();
+
+    let doneContainer = document.createElement('div');
+    body.appendChild(doneContainer);
+    doneContainer.classList.add("done-container");
+
+    let doneImg = document.createElement('img');
+    doneImg.src = 'success_check.svg';
+    doneContainer.appendChild(doneImg);
+    doneImg.classList.add("success-check-lg");
+
+    let doneText = document.createElement('span');
+    doneContainer.appendChild(doneText);
+    doneText.classList.add("done-text");
+    doneText.textContent = "Done!";
 }
